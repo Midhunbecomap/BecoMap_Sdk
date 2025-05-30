c7fae8b2a16694a91f2b6e1d42dced962d043177
Midhunbecomap <midhun@becomap.com>
2025-05-30 17:41:11 +0530

    now showing events

--- app/src/main/java/com/example/becomap_android_new/ui/MainActivity.java
+++ app/src/main/java/com/example/becomap_android_new/ui/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.becomap_android_new.ui;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.core.view.WindowCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
@@ -15,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
         setContentView(R.layout.activity_main);
 
         BottomNavigationView navView = findViewById(R.id.bottom_navigation);
